dc6f3cac282a2f99cd449886fdffbb856b22c959
Ttimmms <thudtim04@gmail.com>
2025-09-19 16:51:52 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Define port with fallback
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on: ${PORT}`);
